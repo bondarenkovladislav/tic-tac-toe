1bda0f016a22b5930c62d99447c016fbb267074d
vladislavbondarenko <bondarencko40@gmail.com>
2020-06-20 21:26:10 +0300

    Router Controller

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,4 +1,10 @@
-import { BrowserRouter as Router, Route, Switch, useHistory, withRouter } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  useHistory,
+  withRouter,
+} from 'react-router-dom'
 import React, { useEffect } from 'react'
 import { Home } from './Home'
 import { LeaderBoardPage } from './LeaderBoardPage'
@@ -6,32 +12,22 @@ import { TempPage } from './TempPage'
 import { AuthorisationForm } from './AuthorisationForm'
 import { ResultsPage } from './ResultsPage'
 import { WaitPage } from './WaitPage'
+import { RouterController } from './RouterController'
 
-
-
-export const Main_ = (props:any) => {
-
-  useEffect(() => {
-    if (localStorage.getItem('key') != null) {
-    props.history.push('/wait')
-    }
-    else {
-    props.history.push('/login')
-    }
-    },[])
-
+export const Main_ = (props: any) => {
   return (
     <Router>
       <Switch>
-        <Route exact path={'/'} component={TempPage} />
-        <Route exact path="/settings" component={Home} />
-        <Route exact path={'/leaderboard'} component={LeaderBoardPage} />
-        <Route exact path={'/login'} component={AuthorisationForm} />
-        <Route exact path={'/results'} component={ResultsPage} />
-        <Route exact path={'/wait'} component={WaitPage} />
+        <RouterController>
+          <Route exact path={'/'} component={TempPage} />
+          <Route exact path="/settings" component={Home} />
+          <Route exact path={'/leaderboard'} component={LeaderBoardPage} />
+          <Route exact path={'/login'} component={AuthorisationForm} />
+          <Route exact path={'/results'} component={ResultsPage} />
+          <Route exact path={'/wait'} component={WaitPage} />
+        </RouterController>
       </Switch>
     </Router>
-
   )
 }
 
